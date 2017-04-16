7ea8d8a3c61597d92e408c3057e599dbd4fa8e1f
andela-ofaith <faith.omokaro@andela.com>
2017-04-16 08:27:09 +0100

    create model, controller, migration files

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ const documentRoute = require('./server/routes/DocumentsRoutes')(app);
 const roleRoute = require('./server/routes/DocumentsRoutes')(app);
 
 
-const server = app.listen(3000, () => {
-  console.log('Hi I am running at 127.0.0.1:3000');
+const server = app.listen(8000, () => {
+  console.log('Hi I am running at 127.0.0.1:8000');
 });
