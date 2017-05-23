96665f76ae793d7c490a05388443f7c848ff92dc
andela-ofaith <faith.omokaro@andela.com>
2017-05-23 15:04:37 +0100

    update index file

--- client/utils/index.js
+++ client/utils/index.js
@@ -3,7 +3,7 @@ import { getToken } from './helpers';
 
 const api = (AUTH_TOKEN = '', BASE_URL) => {
   const instance = axios.create({
-    baseURL: BASE_URL || 'http://localhost:8009',
+    baseURL: BASE_URL || '',
   });
 
   // Adds interceptors to every request
