774cb39e3b98c78a147583891f1199b82319b9d7
andela-ofaith <faith.omokaro@andela.com>
2017-05-30 17:00:04 +0100

    modify SERT_CURRENT_USER reducer

--- client/reducers/login.js
+++ client/reducers/login.js
@@ -8,10 +8,10 @@ const initialState = {
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case SET_CURRENT_USER:
-      return {
+      return Object.assign({}, state, {
         isAuthenticated: !isEmpty(action.user),
         user: action.user
-      };
+      });
     default: return state;
   }
 };
