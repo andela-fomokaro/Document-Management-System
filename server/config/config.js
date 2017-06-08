ba6747cb20118a65923e89532cb2d8bbef4bfa85
andela-ofaith <faith.omokaro@andela.com>
2017-06-08 14:49:00 +0100

    add client-side test

--- server/config/config.js
+++ server/config/config.js
@@ -7,19 +7,19 @@ module.exports = {
     dialect: 'postgres',
     logging: false
   },
-  test: {
-    username: 'andelaeveloper',
-    password: null,
-    database: 'testSystem',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-    logging: false
-  },
   // test: {
-  //   use_env_variable: 'DATABASE_TEST_URL',
+  //   username: 'andelaeveloper',
+  //   password: null,
+  //   database: 'testSystem',
+  //   host: '127.0.0.1',
   //   dialect: 'postgres',
   //   logging: false
   // },
+  test: {
+    use_env_variable: 'DATABASE_TEST_URL',
+    dialect: 'postgres',
+    logging: false
+  },
   production: {
     use_env_variable: 'BACKUP_DATABASE'
   }
